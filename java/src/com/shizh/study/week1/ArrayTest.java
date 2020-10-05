9685fef4c5af77334dc1a8d238ddf915716a5501
shizihui <shi56535874@163.com>
2020-10-06 00:56:18 +0800

    课堂作业

---
+++ java/src/com/shizh/study/week1/ArrayTest.java
@@ -0,0 +1,63 @@
+package com.shizh.study.week1;
+import com.sun.org.apache.xerces.internal.xs.ItemPSVI;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @ClassName maxmin
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/5
+ **/
+
+public  class ArrayTest {
+      static  int Max = 5;
+        public static void main(String[] args) {
+            int sum = 0;
+            int[] a = new int[Max];
+            //创建随机数
+            Random Random = new Random();
+            //数组原始值
+            for (int i = 0; i < Max; i++) {
+                a[i] = Random.nextInt(10);
+                //循环生成100以内的随机整数赋值给数组
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            int max = a[0];
+            int min = a[0];
+            for (int i = 0; i < Max; i++) {
+                if (a[i] > max) {
+                    max = a[i];
+                }
+                if (a[i] < min) {
+                    min = a[i];
+                }
+
+            }
+
+            System.out.println("最大值是:" + max);
+            System.out.println("最小值是:" + min);
+            sum = max + min;
+            System.out.println("最大值和最小值的和:" + sum);
+
+
+            for (int i = 0; i < a.length; i++) {
+                for (int j = i+1; j < a.length; j++){
+                    if (a[i] > a[j ]) {
+                        int temp = a[i];
+                        a[i] = a[j ];
+                        a[j] = temp;
+                    }
+
+                }
+                System.out.print(a[i]+" ");
+             }
+
+        }
+
+}
+
+
+
