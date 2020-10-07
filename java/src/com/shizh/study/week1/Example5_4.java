3f3328bc267d153eedc62e6491955ef460363154
shizihui <shi56535874@163.com>
2020-10-07 22:10:53 +0800

    十月七号

---
+++ java/src/com/shizh/study/week1/Example5_4.java
@@ -0,0 +1,25 @@
+package com.shizh.study.week1;
+
+/**
+ * @ClassName Example5_4
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/7
+ **/
+
+    class XiyoujiRenwu{
+        float height, weight;
+        String head, ear;
+        void speak(String s){
+            System.out.println("s");
+
+        }
+    }
+public class Example5_4 {
+    public static void main(String[] args) {
+        XiyoujiRenwu zhubajie,sunwukong;
+        zhubajie = new XiyoujiRenwu();
+        sunwukong = new XiyoujiRenwu();
+
+    }
+}
