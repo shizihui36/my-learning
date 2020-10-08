6f5b6fb3e00912723101c525b3421ce2caa2f89c
shizihui <shi56535874@163.com>
2020-10-08 17:43:40 +0800

    十月八号

---
+++ java/src/com/shizh/study/week1/PersonName.java
@@ -0,0 +1,12 @@
+package com.shizh.study.week1;
+
+/**
+ * @ClassName PersonName
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/8
+ **/
+public enum  PersonName {
+        唐僧,悟空,八戒,沙僧
+    }
+
