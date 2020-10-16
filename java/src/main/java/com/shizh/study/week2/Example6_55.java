8d55d97f0bc7dc5847d81574167b1b76568a0139
shizihui <shi56535874@163.com>
2020-10-16 22:03:54 +0800

    10.16

---
+++ java/src/main/java/com/shizh/study/week2/Example6_55.java
@@ -0,0 +1,16 @@
+package com.shizh.study.week2;
+
+/**
+ * @ClassName Example6_55
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/16
+ **/
+import java.util.Date;
+public class Example6_55 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("本地机器的时间:");
+        System.out.println(date);
+    }
+}
