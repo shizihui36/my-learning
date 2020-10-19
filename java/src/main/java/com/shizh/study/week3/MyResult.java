92478cc5ba6e9c16f06a6106b7796501410aa0e2
shizihui <shi56535874@163.com>
2020-10-19 18:56:56 +0800

    10.19

---
+++ java/src/main/java/com/shizh/study/week3/MyResult.java
@@ -0,0 +1,13 @@
+package com.shizh.study.week3;
+
+/**
+ * @ClassName MyResult
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/19
+ **/
+public class MyResult {
+    private Integer max;
+    private Integer min;
+    private Integer sum;
+}
