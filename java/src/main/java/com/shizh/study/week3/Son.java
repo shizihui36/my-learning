94ebc360e95f6e9971d34844e8d4979b518444fc
shizihui <shi56535874@163.com>
2020-10-20 21:51:51 +0800

    10.20

---
+++ java/src/main/java/com/shizh/study/week3/Son.java
@@ -0,0 +1,15 @@
+package com.shizh.study.week3;
+
+/**
+ * @ClassName Son
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/20
+ **/
+
+public class Son extends Father {
+    public String hand;
+    public String getHand(){
+        return hand;
+    }
+}
