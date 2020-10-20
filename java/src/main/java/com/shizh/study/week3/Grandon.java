75d79d0b7e5a7318afa46fc3b5d83ca889f21ba3
shizihui <shi56535874@163.com>
2020-10-20 21:58:22 +0800

    10.20

---
+++ java/src/main/java/com/shizh/study/week3/Grandon.java
@@ -0,0 +1,11 @@
+package com.shizh.study.week3;
+
+/**
+ * @ClassName Grandon
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/20
+ **/
+public class Grandon extends Son{
+    public String foot;
+}
