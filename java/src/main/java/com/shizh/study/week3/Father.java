4a2f4c46afc96eca853e54b0956c9e8d15daf142
shizihui <shi56535874@163.com>
2020-10-20 21:55:17 +0800

    10.20

---
+++ java/src/main/java/com/shizh/study/week3/Father.java
@@ -0,0 +1,14 @@
+package com.shizh.study.week3;
+
+/**
+ * @ClassName Father
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/20
+ **/
+
+public class Father {
+    private int money;
+    protected int height;
+    int weight;
+}
