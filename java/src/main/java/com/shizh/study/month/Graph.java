079dd689e2eddddb510477fbf55dcaa835d6d057
shizihui <shi56535874@163.com>
2020-10-24 18:57:30 +0800

    10.24

---
+++ java/src/main/java/com/shizh/study/month/Graph.java
@@ -0,0 +1,12 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName Graph
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/24
+ **/
+abstract class Graph {
+    public  abstract double getCircumference();
+    public abstract double getArea();
+}
