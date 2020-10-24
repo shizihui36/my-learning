7df1850b88136c7d728a8b484295a244b770041a
shizihui <shi56535874@163.com>
2020-10-24 18:55:33 +0800

    10.24

---
+++ java/src/main/java/com/shizh/study/month/BaseGraph.java
@@ -0,0 +1,13 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName BaseGraph
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/24
+ **/
+abstract class BaseGraph {
+    //图形的周长
+
+    public abstract double getCircumference();
+}
