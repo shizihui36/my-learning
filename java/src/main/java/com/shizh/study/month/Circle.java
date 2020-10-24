78468373706d6c515781b039d70d1869bdb42c38
shizihui <shi56535874@163.com>
2020-10-24 18:56:25 +0800

    10.24

---
+++ java/src/main/java/com/shizh/study/month/Circle.java
@@ -0,0 +1,20 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName Circle
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/24
+ **/
+public class Circle extends BaseGraph {
+     private  final double r;
+
+     public Circle (double r){
+         super();
+         this.r = r;
+     }
+     @Override
+    public double getCircumference(){
+         return 2 * r * Math.PI;
+     }
+}
