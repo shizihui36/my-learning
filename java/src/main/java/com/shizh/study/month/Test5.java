b7425d844b30db1d0be7e4869c3cfe6656e9031b
shizihui <shi56535874@163.com>
2020-10-27 21:56:00 +0800

    10.27

---
+++ java/src/main/java/com/shizh/study/month/Test5.java
@@ -0,0 +1,24 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName Test5
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/27
+ **/
+public class Test5 {
+    public static void main(String[] args) {
+        int a1 = 10;
+        int a2 = 11;
+        boolean b1 = a1 % 2 == 0;
+        boolean b2 = a2 % 2 == 0;
+        System.out.println("10是偶数？"+b1);
+        System.out.println("11是偶数？"+b2);
+         int a3 = 12;
+         int a4 = 13;
+         boolean b3 = a3 % 2 == 1;
+         boolean b4 = a4 % 2 == 1;
+        System.out.println("12是奇数？"+b3);
+        System.out.println("13是奇数？"+b4);
+    }
+}
