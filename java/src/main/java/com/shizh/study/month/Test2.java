e73bd20c0b1209fb5dc31036274a1f4946795097
shizihui <shi56535874@163.com>
2020-10-27 21:53:49 +0800

    10.27

---
+++ java/src/main/java/com/shizh/study/month/Test2.java
@@ -0,0 +1,24 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/27
+ **/
+public class Test2 {
+    public static void main(String[] args) {
+         byte b1 = 10;
+         byte b2 = 20;
+         int b3 = b1 + b2;
+        System.out.println("b1和b2的和:"+ b3);
+        short s1 = 1000;
+        short s2 = 2000;
+        int s3 = s1 + s2;
+        System.out.println("s1和s2的和"+s3);
+        char c1 = 'a';
+        int i1 = 30;
+        int ch3 = c1 -i1 ;
+        System.out.println("c1和i1的差:"+ch3);
+    }
+}
