6148d838c4fdf94ad113cf816b49b3163cc15f28
shizihui <shi56535874@163.com>
2020-10-27 21:54:32 +0800

    10.27

---
+++ java/src/main/java/com/shizh/study/month/Test3.java
@@ -0,0 +1,28 @@
+package com.shizh.study.month;
+
+/**
+ * @ClassName Test3
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/10/27
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+        int i1 = 100;
+        long l1 = 200;
+        long add = i1 + l1;
+        System.out.println("add的值:"+add);
+        long l2 = 1000000;
+        float f2 = 0.45F;
+        float add2 = l2 + f2;
+        System.out.println("add2的值:"+add2);
+        int i3 = 1000000;
+        double d3 = 0.45;
+        double add3 = i3 + d3;
+        System.out.println("add3的值:"+add3);
+        float f4 = 1000000.789F;
+        double d4 = 0.45;
+        double add4 = f4 + d4;
+        System.out.println("add4的值:"+add4);
+    }
+}
