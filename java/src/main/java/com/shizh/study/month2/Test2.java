0272b9b944a6aa0f3e61d5d0f10eae4b4c4e1093
shizihui <shi56535874@163.com>
2020-11-04 20:35:20 +0800

    11.4

---
+++ java/src/main/java/com/shizh/study/month2/Test2.java
@@ -0,0 +1,15 @@
+package com.shizh.study.month2;
+
+/**
+ * @ClassName Test2
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/11/4
+ **/
+public class Test2 {
+        public static void main(String[] args) {
+            Circle circle = new Circle(8);
+            circle.showArea();
+            circle.showPerimeter();
+        }
+}
