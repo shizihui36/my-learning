ec7a3a1ba58e8607eddc16c4ec24a0d1763c7099
shizihui <shi56535874@163.com>
2020-11-04 20:37:15 +0800

    11.4

---
+++ java/src/main/java/com/shizh/study/month2/Test3.java
@@ -0,0 +1,14 @@
+package com.shizh.study.month2;
+
+/**
+ * @ClassName Test3
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/11/4
+ **/
+public class Test3 {
+    public static void main(String[] args) {
+      Card card = new Card("黑桃","A");
+      card.showCard();
+    }
+}
