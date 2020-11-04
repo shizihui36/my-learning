5e4c896acc193f553a4193f8ebd706f0b3d02555
shizihui <shi56535874@163.com>
2020-11-04 20:45:31 +0800

    11.4

---
+++ java/src/main/java/com/shizh/study/month2/IBookException.java
@@ -0,0 +1,17 @@
+package com.shizh.study.month2;
+
+
+
+/**
+ * @ClassName IBookException 书名异常
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/11/2
+ **/
+
+public class IBookException extends Exception {
+
+    public IBookException(String message) {
+        super(message);
+    }
+}
