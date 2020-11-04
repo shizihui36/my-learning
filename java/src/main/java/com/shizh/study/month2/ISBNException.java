6647b3056d40c7934834da4a3382844d97939190
shizihui <shi56535874@163.com>
2020-11-04 20:46:15 +0800

    11.4

---
+++ java/src/main/java/com/shizh/study/month2/ISBNException.java
@@ -0,0 +1,13 @@
+package com.shizh.study.month2;
+
+/**
+ * @ClassName IsbnException
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/11/2
+ **/
+public class ISBNException extends Exception {
+    public  ISBNException (String message){
+        super(message);
+    }
+}
