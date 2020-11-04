7867de90884fef822990b2a6b9a8b1212d327885
shizihui <shi56535874@163.com>
2020-11-04 20:46:41 +0800

    11.4

---
+++ java/src/main/java/com/shizh/study/month2/PriceException.java
@@ -0,0 +1,13 @@
+package com.shizh.study.month2;
+
+/**
+ * @ClassName PriceException
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2020/11/2
+ **/
+public class PriceException extends Exception {
+    public PriceException(String message){
+        super(message);
+    }
+}
